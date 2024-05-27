Updated ptr filter with latest discussions with A. Bohe and F. Nouguier: not includes 9 beams with "typical" weighting. Still uses fixed spacing along-track and fixed Vsat ... Included option to restrict orbital velocity for cut off estimation to relatively shorter components

lib_filters_obp.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 import scipy.constants as consts
 
 def G_field_oneway(theta):
-    # question for Alejandro: why 2.25 ? ... when **4 this gives another exp factor ... 
-    return np.exp(-np.abs(theta/(np.radians(.107)/2))**2.25*np.log(2))
+    # This antenna pattern function was fitted on measured antenna patterns by Alejandro Bohe (hence the "strange" 2.25 power)
+    # this is for the field (hence the sqrt, because the fit was on the "power") . This was corrected on 2024/05/26 
+    return np.sqrt(np.exp(-np.abs(theta/(np.radians(.107)/2))**2.25*np.log(2)))
 
 
 def get_obp_filter(L_filt = 1, sampling_in = 0.025, f_axis = None, plot_flag = True, kernel="parzen"):
@@ -30,7 +31,7 @@ def get_obp_filter(L_filt = 1, sampling_in = 0.025, f_axis = None, plot_flag = T
     if kernel == "sinc2":
         xn=(np.arange(0,Nparzen-1,1)-Nparzen//2)*2*np.pi
         w_obp = (np.sinc(xn*L_filt*0.00222))**2
-    if kernel == "alejandro_azptr":
+    if kernel == "alejandro_azptr_beam0":
        PRF = 4332
        N_burst = 9
        v_sat = 7310.
@@ -47,6 +48,40 @@ def get_obp_filter(L_filt = 1, sampling_in = 0.025, f_axis = None, plot_flag = T
        y_tab = np.arange(-200,201)*d+1.0e-3
        ant = G_field_oneway(y_tab/H)**4
        w_obp[:len(y_tab)] = np.sin(2*np.pi*v_sat*N_burst/(lambda_c*H*PRF)*y_tab)**2/np.sin(2*np.pi*v_sat/(lambda_c*H*PRF)*y_tab)**2*ant
+       w_obp /= np.sum(w_obp)*d
+       sampling_in=d/1000.
+    if kernel == "alejandro_azptr":
+       PRF = 4332
+       N_burst = 9
+       v_sat = 7310.
+       H = 875.0e3
+       f_s = 300.0e6
+       v_ground = v_sat/(1+H/6377.0e3)
+       lambda_c = (consts.c/35.75e9)
+       incidence_deg = 3.5 # just used for the ground/range conversion for xtrack
+       d = N_burst*v_ground/PRF # along track posting before OBP averaging
+       w_ptr = np.zeros(512)
+       w_ptr[:401] = np.sinc(2*v_sat*N_burst/(lambda_c*H*PRF)*np.arange(-200,201)*d)**2
+       w_ptr /= np.sum(w_ptr)*d
+       w_obp = np.zeros(512)
+       y_tab = np.arange(-200,201)*d+1.0e-3
+       ant = G_field_oneway(y_tab/H)**4
+#       w_obp[:len(y_tab)] = np.sin(2*np.pi*v_sat*N_burst/(lambda_c*H*PRF)*y_tab)**2/np.sin(2*np.pi*v_sat/(lambda_c*H*PRF)*y_tab)**2*ant
+       # Defines the PTR for each of the 9 beams
+       w_ale9 = np.zeros(512)
+       w_ptrantana_b = np.zeros((9,512))
+       w_ptrantana_c = np.zeros((9,512))
+       b_vals = np.arange(-4,5)
+       arg_b = 0.8/N_burst*b_vals[:,None]+2*v_sat/(lambda_c*H*PRF)*y_tab[None,:]
+       w_ptrantana_b[:,:len(y_tab)] = np.sin(np.pi*N_burst*arg_b)**2/np.sin(np.pi*arg_b)**2*(G_field_oneway(y_tab/H)**4)[None,:]
+       w_ptrantana_b = w_ptrantana_b/(np.sum(w_ptrantana_b, axis=1)[:,None]*d)
+       weights = np.array([0.07019711, 0.09767803, 0.12220272, 0.13814296, 0.14355835,0.13814296, 0.12220272, 0.09767803, 0.07019711])
+       for ib in range(9) :
+          w_ale9=w_ale9+weights[ib]*np.roll(w_ptrantana_b[ib,:],14*(ib-4))
+          w_ptrantana_c[ib,:] = np.roll(w_ptrantana_b[ib,:],14*(ib-4))
+       # these weights actually vary along the track. This is one set of weights obtained by averaging along one (?) cycle
+       w_obp[:len(y_tab)]=w_ale9[:len(y_tab)]
+        
        w_obp /= np.sum(w_obp)*d
        sampling_in=d/1000.
 
@@ -79,6 +114,80 @@ def get_obp_filter(L_filt = 1, sampling_in = 0.025, f_axis = None, plot_flag = T
     
     return x_axis, w_obp, f_obp, S_obp
 
+
+def get_obp_filter9(L_filt = 1, sampling_in = 0.025, f_axis = None, plot_flag = True, kernel="parzen"):
+    """
+    Get the kernel shape and the spectral response of the filter type specified in the arguments
+    
+    Arguments:
+    L_filt,      Kernel length in km
+    sampling_in, Spatial spacing (in km) of the filter kernel
+    f_axis,      Frequency axis where to compute the spectral response of the filter
+    plot_flag,   Plot filter kernel and spectral response
+    kernel,      Kernel to compute ('parzen' or 'bharris' -for Blackman-Harris)
+    """
+    Nparzen = int(np.round(L_filt/sampling_in))
+    if Nparzen%2 == 0:
+        Nparzen += 1    
+    print("Nb of points OBP kernel: %d" % Nparzen)
+    weights = np.array([0.07019711, 0.09767803, 0.12220272, 0.13814296, 0.14355835,0.13814296, 0.12220272, 0.09767803, 0.07019711])
+    PRF = 4332
+    N_burst = 9
+    v_sat = 7310.
+    H = 875.0e3
+    f_s = 300.0e6
+    v_ground = v_sat/(1+H/6377.0e3)
+    lambda_c = (consts.c/35.75e9)
+    incidence_deg = 3.5 # just used for the ground/range conversion for xtrack
+    d = N_burst*v_ground/PRF # along track posting before OBP averaging
+    w_ptr = np.zeros(512)
+    y_tab = np.arange(-200,201)*d+1.0e-3
+    ant = G_field_oneway(y_tab/H)**4
+    b_vals = np.arange(-4,5)
+    arg_b = 0.8/N_burst*b_vals[:,None]+2*v_sat/(lambda_c*H*PRF)*y_tab[None,:]
+    w_ptrantana_b = np.zeros((9,512))
+    w_ptrantana_c = np.zeros((9,512))
+    w_obp = np.zeros(512)
+    # Defines the PTR for each of the 9 beams
+    w_ptrantana_b[:,:len(y_tab)] = np.sin(np.pi*N_burst*arg_b)**2/np.sin(np.pi*arg_b)**2*(G_field_oneway(y_tab/H)**4)[None,:]
+    w_ptrantana_b = w_ptrantana_b/(np.sum(w_ptrantana_b, axis=1)[:,None]*d)
+    for ib in range(9) :
+       w_obp[:len(y_tab)]=w_ptrantana_b[ib,:len(y_tab)]
+        
+       sampling_in=d/1000.
+       w_obp /= np.sum(w_obp)
+       x_axis = np.arange(-(len(w_obp)-1)/2, (len(w_obp)-1)/2 + 1)*sampling_in
+
+    # OBP spectrum
+    
+       if type(f_axis) != np.ndarray:
+        if f_axis == None:
+            f_obp, S_obp = sg.freqz(w_obp, fs=1/sampling_in)
+        else:
+            f_obp, S_obp = sg.freqz(w_obp, fs=1/sampling_in, worN=f_axis)
+       else:
+        f_obp, S_obp = sg.freqz(w_obp, fs=1/sampling_in, worN=f_axis)
+       if ib== 0:
+         S_obpsum=weights[ib]*S_obp
+       else :
+         S_obpsum=S_obpsum+weights[ib]*S_obp  
+
+
+    S_obp = np.abs(S_obpsum)**2
+
+    if plot_flag:
+        fig, ax = plt.subplots(1, 2, figsize=(11, 4))
+        ax[0].plot(x_axis, w_obp/np.max(w_obp), ".-")
+        ax[1].plot(f_obp, 10*np.log10(S_obp))
+        ax[0].grid()
+        ax[0].set_xlabel("[km]")
+        ax[0].set_title("%s window (not normalized)" % kernel)
+        ax[1].grid()
+        ax[1].set_xlabel("freq [1/km]")
+        ax[1].set_ylabel("dB [m**2.km]")
+        plt.show()
+    
+    return x_axis, w_obp, f_obp, S_obp
 
 
 def compute_aliased_spectrum_2D(fx_in, fy_in, S_in, fsx, fsy, nrep=2):

swot_swell_utils.py
@@ -224,7 +224,7 @@ def SWOTdefine_swell_mask_simple(Eta,coh,ang,medsig0,dlat,kx2,ky2,cohthr=0.3,cfa
        cohOK=0
        amask=Etam/10*np.sign(ky2m*dlat)
        maskset=2
-    if mask_choice == -1:
+    if (cohOK==1) or (mask_choice == -1):
        Emax=2/np.max(Etam.flatten())
        amask=Etam*Emax*np.sign(-ky2m)
        maskset=3
@@ -305,16 +305,17 @@ def SWOTdefine_swell_mask(mybox,mybos,flbox,dy,dx,nm,mm,Eta,coh,ang,dlat,mask_ch
     if ncoh2 > 3:
        ncoh=ncoh2
     if cohthr > 0.8/cfac and ncoh > 3 and (np.nanmedian(mybos) < 70):
-       cohs=(cohm/cohthr2)*np.sign(np.cos(angm))*np.sign(ky2m*dlat)
+       cohs=(cohm/cohthr)*np.sign(np.cos(angm))*np.sign(ky2m*dlat)
        amask=ndimage.binary_dilation((cohs > 1 ).astype(int))
        maskset=1
     else:
        cohOK=0
        amask=Etam/10*np.sign(ky2m*dlat)
        maskset=2
-    if mask_choice == -1:
+    if (cohOK==1) or (mask_choice == -1):
        Emax=2/np.max(Etam.flatten())
        amask=Etam*Emax*np.sign(-ky2m)
+       amask=ndimage.binary_dilation((amask > 0.5).astype(int)) 
        maskset=3
     if (mask_choice == -2):
        Emax=10/np.max(Etam.flatten())

wave_physics_functions.py
@@ -63,7 +63,7 @@ def  wavespec_Efth_to_Ekxky(eft1s,fren,dfreq,dirn,dth,dkx=0.0001,dky=0.0001,nkx=
     return Ekxky,kx,ky,kx2,ky2
 
 #############################################################################
-def  wavespec_Efth_to_first3(efth,fren,dfreq,dirn,dth)  :
+def  wavespec_Efth_to_first3(efth,fren,dfreq,dirn,dth,cut=1E4)  :
     '''
     Computes first 3 moments from E(f,theta) spectrum
     inputs :
@@ -75,12 +75,14 @@ def  wavespec_Efth_to_first3(efth,fren,dfreq,dirn,dth)  :
     d2r=np.pi/180
     grav=9.81
     wn=(2*np.pi*fren)**2/grav
+    wavelength=2*np.pi/wn
     Cg=grav/(4*np.pi*fren) 
     dk=2*np.pi*dfreq/Cg
     [nf,nt]=np.shape(efth)
     dir2=np.tile((dirn*d2r).reshape(1,nt),(nf,1))
     Ef=np.sum(efth,             axis=1)*dth
-    Etot=np.sum(Ef*dfreq)
+    inds=np.where(wavelength < cut)[0]
+    Etot=np.sum(Ef[inds]*dfreq[inds])
     eftn=0.5*(efth+np.roll(efth,nt//2,axis=1))
 
     #a1E=np.sum(efth*np.cos(dir2),axis=1)*dth
@@ -101,8 +103,9 @@ def  wavespec_Efth_to_first3(efth,fren,dfreq,dirn,dth)  :
     #   print('ft:',ind,fren[ind],wn[ind],'Cg:',Cg[ind],dfreq[ind]/(wn[ind]*dk[ind]),grav**2/(2*((np.pi*2)**4*fren[ind]**3 )) )
     Qkk=np.sqrt(Q2)/Etot
     Qf=np.sqrt(np.sum(Ef**2*dfreq))/Etot
-    Tm0m1=np.sum(Ef*dfreq/fren)/Etot
-    Em2=np.sum(Ef*dfreq*fren**2)+Ef[-1]*dfreq[-1]*0.5*fren[-1]**3  # integral including f^-5 tail 
+
+    Tm0m1=np.sum(Ef[inds]*dfreq[inds]/fren[inds])/Etot
+    Em2=np.sum(Ef[inds]*dfreq[inds]*fren[inds]**2)+Ef[-1]*dfreq[-1]*0.5*fren[-1]**3  # integral including f^-5 tail 
     Tm02=np.sqrt(Etot/Em2)
     Hs=4*np.sqrt(Etot) 
   #     print('WHAT:',ind,fren[ind],Ef[ind],a1[ind],b1[ind],m1[ind],efth[ind,:])