78351ee34cc29d6ad41fefb99ca8997dba5a8afe
ardhuin <ardhuin@ifremer.fr>
2026-06-25 11:39:28 +0200

    Added notebooks related to paper on SWOT SSH noise

---
+++ scripts/paths.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+# --- Data directories ---
+# Raw/processed/output folders
+ssh_Unsmoothed_D_folder=Path('/media/ardhuin/FabBack/SWOT2/Unsmoothed/')
+swot_WindWave_L2_D_folder=Path('/media/ardhuin/FabBack/SWOT2/Basic_D/')
+swot_Basic_L2_D_folder=Path('/media/ardhuin/FabLinux/SWOT2/Basic_D/')
+swot_spectra_folder=Path('/media/ardhuin/FabLinux/SWOT2/')
+
+wavemodel_CCI_folder=Path('/media/ardhuin/FabLinux/WW3-CCI/')
+wavemodel_GLOBIG_folder=Path('/media/ardhuin/FabLinux/WW3-SWOT/GLOBIG/')
